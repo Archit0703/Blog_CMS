5f244d0f3b09bf845f3fa10d7f400556618d34fa
Archit Waghmode <153592065+Archit0703@users.noreply.github.com>
2025-07-05 16:49:16 +0530

    Update User.js

--- User.js
+++ User.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
     trim: true,
     minlength: [3, 'Username must be at least 3 characters long'],
     maxlength: [30, 'Username cannot exceed 30 characters']
-  },
+  }, 
   email: {
     type: String,
     required: [true, 'Email is required'],
