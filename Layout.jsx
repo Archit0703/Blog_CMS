a4943a09b52adfcb737116f2134f0b90703545da
Archit Waghmode <153592065+Archit0703@users.noreply.github.com>
2025-07-28 20:35:55 +0530

    Update Layout.jsx

--- Layout.jsx
+++ Layout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
-import Footer from './Footer';
-
+import footer from '.Footer';
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
